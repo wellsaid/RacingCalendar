1cc81c6d7e42326b752c393fc92a46a759597e56
wellsaid <benedetto.girgenti@protonmail.com>
2018-06-24 16:21:59 +0200

    Modified database to have all fields in CamelCase

--- restful_api/database_creation.sql
+++ restful_api/database_creation.sql
@@ -7,48 +7,48 @@ DROP TABLE IF EXISTS series;
 DROP TABLE IF EXISTS series_types;
 
 CREATE TABLE series_types (
-	short_name VARCHAR(20) PRIMARY KEY,
-	complete_name VARCHAR(50),
+	shortName VARCHAR(20) PRIMARY KEY,
+	completeName VARCHAR(50),
 	description VARCHAR(500),
-	thumbnail_URL VARCHAR(100)
+	thumbnailURL VARCHAR(100)
 );
 
 CREATE TABLE series (
-	short_name VARCHAR(10) PRIMARY KEY,
-	complete_name VARCHAR(50) NOT NULL,
-	series_type VARCHAR(20) NOT NULL,
+	shortName VARCHAR(10) PRIMARY KEY,
+	completeName VARCHAR(50) NOT NULL,
+	seriesType VARCHAR(20) NOT NULL,
 	description VARCHAR(300),
-	logo_URL VARCHAR(100),
-	thumbnail_URL VARCHAR(100),
-	FOREIGN KEY (series_type) REFERENCES series_types(short_name)
+	logoURL VARCHAR(100),
+	thumbnailURL VARCHAR(100),
+	FOREIGN KEY (series_types) REFERENCES series_types(shortName)
 );
 
 CREATE TABLE events (
 	ID INTEGER,
-	series_short_name VARCHAR(10),
-	event_short_name VARCHAR(10),
-	event_name VARCHAR(50),
-	circuit_name VARCHAR(50),
-	start_date DATE,
-	end_date DATE,
-	FOREIGN KEY (series_short_name) REFERENCES series(short_name),
-	PRIMARY KEY (ID, series_short_name)
+	seriesShortName VARCHAR(10),
+	eventShortName VARCHAR(10),
+	eventName VARCHAR(50),
+	circuitName VARCHAR(50),
+	startDate DATE,
+	endDate DATE,
+	FOREIGN KEY (series_short_name) REFERENCES series(shortName),
+	PRIMARY KEY (ID, seriesShortName)
 );
 
 CREATE TABLE session_types (
-	short_name VARCHAR(10) PRIMARY KEY,
-	complete_name VARCHAR(50) NOT NULL
+	shortName VARCHAR(10) PRIMARY KEY,
+	completeName VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE sessions (
-	short_name VARCHAR(10) NOT NULL,
-	complete_name VARCHAR(50) NOT NULL,
-	session_type VARCHAR(10) NOT NULL,
-	event_ID INTEGER NOT NULL,
-	series_short_name VARCHAR(10) NOT NULL,
-	start_datetime DATETIME NOT NULL,
-	end_datetime DATETIME,
-	FOREIGN KEY (session_type) REFERENCES session_types(short_name),
-	FOREIGN KEY (event_ID, series_short_name) REFERENCES events(ID, series_short_name),
-	PRIMARY KEY (short_name, event_ID, series_short_name)
+	shortName VARCHAR(10) NOT NULL,
+	completeName VARCHAR(50) NOT NULL,
+	sessionType VARCHAR(10) NOT NULL,
+	eventID INTEGER NOT NULL,
+	seriesShortName VARCHAR(10) NOT NULL,
+	startDateTime DATETIME NOT NULL,
+	endDateTime DATETIME,
+	FOREIGN KEY (session_type) REFERENCES session_types(shortName),
+	FOREIGN KEY (event_ID, seriesShortName) REFERENCES events(ID, seriesShortName),
+	PRIMARY KEY (shortName, eventID, seriesShortName)
 );
