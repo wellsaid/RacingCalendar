3b88c6136b2d0f32f5cdb08a7b611488804cbd4f
wellsaid <benedetto.girgenti@protonmail.com>
2018-06-27 20:22:36 +0200

    FIX: Removed old libraries from settings.gradle + Finished writing Daos for the database

--- android_app/RacingCalendar/RacingCalendarData/src/main/java/wellsaid/it/racingcalendardata/RacingCalendarDaos.java
+++ android_app/RacingCalendar/RacingCalendarData/src/main/java/wellsaid/it/racingcalendardata/RacingCalendarDaos.java
@@ -26,4 +26,67 @@ public class RacingCalendarDaos {
         void delete(SeriesType seriesType);
     }
 
+    @Dao
+    public interface SeriesDao {
+        @Query("SELECT * FROM Series")
+        List<Series> getAll();
+
+        @Query("SELECT * FROM Series WHERE shortName IN (:shortName)")
+        Series getByShortName(String shortName);
+
+        @Insert
+        void insert(Series seriesType);
+
+        @Delete
+        void delete(Series seriesType);
+    }
+
+    @Dao
+    public interface EventDao {
+        @Query("SELECT * FROM Event")
+        List<Event> getAll();
+
+        @Query("SELECT * FROM Event WHERE ID IN (:ID) AND seriesShortName IN (:seriesShortName)")
+        Event getByIDAndSeriesShortName(String ID, String seriesShortName);
+
+        @Insert
+        void insert(Event seriesType);
+
+        @Delete
+        void delete(Event seriesType);
+    }
+
+    @Dao
+    public interface SessionTypeDao {
+        @Query("SELECT * FROM SessionType")
+        List<SessionType> getAll();
+
+        @Query("SELECT * FROM SessionType WHERE shortName IN (:shortName)")
+        SessionType getByShortName(String shortName);
+
+        @Insert
+        void insert(SessionType seriesType);
+
+        @Delete
+        void delete(SessionType seriesType);
+    }
+
+    /* @Entity(primaryKeys = {"shortName","eventID","seriesShortName"}) */
+
+    @Dao
+    public interface SessionDao {
+        @Query("SELECT * FROM Session")
+        List<Session> getAll();
+
+        @Query("SELECT * FROM Session WHERE shortName IN (:shortName) " +
+                "AND seriesShortName IN (:seriesShortName)")
+        Event getByIDAndSeriesShortName(String shortName, String eventID, String seriesShortName);
+
+        @Insert
+        void insert(Event seriesType);
+
+        @Delete
+        void delete(Event seriesType);
+    }
+
 }
